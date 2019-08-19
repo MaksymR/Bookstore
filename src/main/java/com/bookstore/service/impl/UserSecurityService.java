028bf23222fd2fb0ca1f6cce0dc553f4f56a40b5
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-19 10:41:24 +0300

    added and created "UserRepository.java" interface to the "com.bookstore.repository"

--- src/main/java/com/bookstore/service/impl/UserSecurityService.java
+++ src/main/java/com/bookstore/service/impl/UserSecurityService.java
@@ -1,6 +1,7 @@
 package com.bookstore.service.impl;
 
 import com.bookstore.domain.User;
+import com.bookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
