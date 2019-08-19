0dda594d28cd894be63a46b99f81133d49cd5691
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-19 11:06:12 +0300

    added and created "SecurityUtility.java" for working with password to the "com.bookstore.utility"

---
+++ src/main/java/com/bookstore/utility/SecurityUtility.java
@@ -0,0 +1,39 @@
+package com.bookstore.utility;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+/*
+ * Such classes are considered as candidates for auto-detection
+ * when using annotation-based configuration and classpath scanning.
+ */
+@Component
+public class SecurityUtility {
+
+    private static final String SALT = "salt"; // Salt should be protected carefully
+
+    // Indicates that a method produces a bean to be managed by the Spring container.
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
+
+    @Bean
+    public static String randomPassword() {
+        String SALTCHARS = "ABCEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+
+        while (salt.length() < 18) {
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
+
+}
