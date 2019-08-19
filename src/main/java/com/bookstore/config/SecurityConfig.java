834f85f3c21af1aad0826e03b422ba2a179a2df4
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-19 10:11:39 +0300

    created "SecurityConfig.java" to the "com.bookstore.config"

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,4 +1,79 @@
 package com.bookstore.config;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/*
+ * Indicates that a class declares one or more {@link Bean @Bean} methods and
+ * may be processed by the Spring container to generate bean definitions and
+ * service requests for those beans at runtime
+ */
+@Configuration
+/*
+ * Controls debugging support for Spring Security
+ */
+@EnableWebSecurity
+/*
+ * Enables Spring Security global method security
+ * */
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /*
+     * Marks a constructor, field, setter method or config method as to be autowired by
+     * Spring's dependency injection facilities.
+     */
+    @Autowired
+    // Interface representing the environment in which the current application is running
+    private Environment env;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
+
+    private BCryptPasswordEncoder passwordEncoder() {
+        return SecurityUtility.passwordEncoder();
+    }
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/css/**",
+            "/js/**",
+            "/image/**",
+            "/",
+            "/myAccount"
+    };
+
+    // the following configuration could be used to register
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().
+                /*	antMatchers("/**").*/
+                        antMatchers(PUBLIC_MATCHERS).
+                permitAll().anyRequest().authenticated();
+
+        http
+                .csrf().disable().cors().disable()
+                .formLogin().failureUrl("/login?error").defaultSuccessUrl("/")
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
+                .and()
+                .rememberMe();
+    }
+
+    @Autowired
+    // building authentication
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+    }
+
 }
