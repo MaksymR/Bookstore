6184809c5f5f75c450ae2207f564a6a9dab9b734
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-20 22:22:01 +0300

    added the "createUser(User user, Set<UserRole> userRoles)"-method in the "com.bookstore.service.UserService.java"-interface for using it in the "newUserPost"-method ("com.bookstore.contorller.HomeController.java"

--- src/main/java/com/bookstore/service/UserService.java
+++ src/main/java/com/bookstore/service/UserService.java
@@ -2,6 +2,9 @@ package com.bookstore.service;
 
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.UserRole;
+
+import java.util.Set;
 
 public interface UserService {
 
@@ -12,5 +15,7 @@ public interface UserService {
     User findByUsername(String username);
 
     User findByEmail(String email);
+
+    User createUser(User user, Set<UserRole> userRoles);
 
 }
