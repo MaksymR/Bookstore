44ea8bcfb658e46e228b5ce46d1cdff385bbc724
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-20 15:58:15 +0300

    crated the "findByUsername(String username)"-method (""com.bookstore.service.UserService.java") and the "findByEmail(String email)"-method ("com.bookstore.service.UserService.java" and "com.bookstore.service.impl.UserServiceImp.java") for the  the "newUserPost()" of the "com.bookstore.controller.HomeController.java"

--- src/main/java/com/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -3,12 +3,17 @@ package com.bookstore.service.impl;
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
 import com.bookstore.repository.PasswordResetTokenRepository;
+import com.bookstore.repository.UserRepository;
 import com.bookstore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
 
     @Autowired
     private PasswordResetTokenRepository passwordResetTokenRepository;
@@ -22,6 +27,15 @@ public class UserServiceImpl implements UserService {
     public void createPasswordResetTokenForUser(final User user, final String token) {
         final PasswordResetToken myToken = new PasswordResetToken(token, user);
         passwordResetTokenRepository.save(myToken);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 
 }
