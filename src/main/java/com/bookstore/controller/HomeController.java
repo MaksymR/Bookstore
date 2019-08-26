6cf04b790b0d7f54bfe214a618965d68810368d9
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-26 04:34:45 +0300

    created the "bookDetail()"-method in the "com.bookstore.controller.HomeController.java"

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -75,8 +75,8 @@ public class HomeController {
              * "Principal" - the abstract notion of a principal, which can be used to represent
              * any entity (user's login)
              */
-            @PathParam("id") Long id, Model model, Principal principal
-    ) {
+            @PathParam("id") Long id, Model model, Principal principal) {
+
         if (principal != null) {
             String username = principal.getName();
             User user = userService.findByUsername(username);
@@ -85,7 +85,15 @@ public class HomeController {
 
         Book book = bookService.findOne(id);
 
+        model.addAttribute("book", book);
+
+        List<Integer> qtyList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        model.addAttribute("qtyList", qtyList);
+        model.addAttribute("qty", 1);
+
         return "bookDetail";
+
     }
 
     /*
@@ -95,8 +103,7 @@ public class HomeController {
     public String forgetPassword(
             HttpServletRequest request,
             @ModelAttribute("email") String email,
-            Model model
-    ) {
+            Model model) {
 
         model.addAttribute("classActiveForgetPassword", true);
 
@@ -149,8 +156,8 @@ public class HomeController {
             HttpServletRequest request,
             @ModelAttribute("email") String userEmail,
             @ModelAttribute("username") String username,
-            Model model
-    ) throws Exception {
+            Model model) throws Exception {
+
         model.addAttribute("classActiveNewAccount", true);
         model.addAttribute("email", userEmail);
         model.addAttribute("username", username);
@@ -216,6 +223,7 @@ public class HomeController {
 
         model.addAttribute("emailSent", true);
         return "myAccount";
+
     }
 
     @RequestMapping("/newUser")
@@ -256,6 +264,7 @@ public class HomeController {
         model.addAttribute("user", user);
         model.addAttribute("classActiveEdit", true);
         return "myProfile";
+
     }
 
 }
