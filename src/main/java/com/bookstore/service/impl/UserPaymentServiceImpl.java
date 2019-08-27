3388cec82280df61b288f6cc331e117bb274e1dc
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-27 22:49:28 +0300

    implemented "com.bookstore.service.UserPaymentService.java"-interface for "updateCreditCard()" in the "com.bookstore.service.impl.UserPaymentServiceImpl.java"

---
+++ src/main/java/com/bookstore/service/impl/UserPaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bookstore.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.UserPayment;
+import com.bookstore.service.UserPaymentService;
+
+@Service
+public class UserPaymentServiceImpl implements UserPaymentService{
+
+    @Autowired
+    private UserPaymentRepository userPaymentRepository;
+
+    @Override
+    public UserPayment findById(Long id) {
+
+        return null;
+
+    }
+
+}
