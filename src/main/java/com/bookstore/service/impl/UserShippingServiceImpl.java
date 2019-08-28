c819a6af7c5b859ddfd1ac6f24124ddb3d6c31cc
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-28 05:07:10 +0300

    added "@Service"-annotation for the "com.bookstore.service.impl.UserShippingServiceImpl.java"

--- src/main/java/com/bookstore/service/impl/UserShippingServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/UserShippingServiceImpl.java
@@ -4,7 +4,9 @@ import com.bookstore.domain.UserShipping;
 import com.bookstore.repository.UserShippingRepository;
 import com.bookstore.service.UserShippingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserShippingServiceImpl implements UserShippingService {
 
     @Autowired
