19c18a4ca424e24fb42d64441d5a1246d97d8801
Maksym Riaboshapka <makmr11@gmail.com>
2019-09-01 22:25:55 +0300

    added the "ShoppingCart"-field to "com.bookstore.domain.User.java" for using it in the "com.bookstore.controller.ShoppingCartController.java"

--- src/main/java/com/bookstore/domain/User.java
+++ src/main/java/com/bookstore/domain/User.java
@@ -30,6 +30,9 @@ public class User implements UserDetails {
     private String phone;
     private boolean enabled = true;
 
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+    private ShoppingCart shoppingCart;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<UserShipping> userShippingList;
 
@@ -124,6 +127,14 @@ public class User implements UserDetails {
 
     public void setUserPaymentList(List<UserPayment> userPaymentList) {
         this.userPaymentList = userPaymentList;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
     }
 
     // Returns the authorities granted to the user
