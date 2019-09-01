01f68515171b042aaff648159e3660d8e7b7e77b
Maksym Riaboshapka <makmr11@gmail.com>
2019-09-01 23:31:25 +0300

    implemented the "com.bookstore.service.ShoppingCartService.java"-interface with "updateShoppingCart()"-method in the "com.bookstore.service.impl.ShoppingCartServiceImpl.java"

--- src/main/java/com/bookstore/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/CartItemServiceImpl.java
@@ -5,9 +5,11 @@ import com.bookstore.domain.ShoppingCart;
 import com.bookstore.repository.CartItemRepository;
 import com.bookstore.service.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CartItemServiceImpl implements CartItemService {
 
     @Autowired
