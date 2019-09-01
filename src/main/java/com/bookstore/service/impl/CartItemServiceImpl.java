cd2b14337107bccc1d6cfe99f78b9f870ef1df80
Maksym Riaboshapka <makmr11@gmail.com>
2019-09-02 01:12:42 +0300

    implemented the "com.bookstore.service.CartItemService.java"-interface and "@Override" "updateCartItem()"-method in the "com.bookstore.service.impl.CartItemServiceImpl.java" for using it in the "com.bookstore.service.impl.ShoppingCartServiceImpl.java"

--- src/main/java/com/bookstore/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/CartItemServiceImpl.java
@@ -7,6 +7,8 @@ import com.bookstore.service.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @Service
@@ -19,4 +21,20 @@ public class CartItemServiceImpl implements CartItemService {
     public List<CartItem> findByShoppingCart(ShoppingCart shoppingCart) {
         return cartItemRepository.findByShoppingCart(shoppingCart);
     }
+
+    @Override
+    public CartItem updateCartItem(CartItem cartItem) {
+
+        BigDecimal bigDecimal = new BigDecimal(cartItem.getBook().getOurPrice())
+                .multiply(new BigDecimal(cartItem.getQty()));
+
+        bigDecimal = bigDecimal.setScale(2, RoundingMode.HALF_UP);
+        cartItem.setSubtotal(bigDecimal);
+
+        cartItemRepository.save(cartItem);
+
+        return cartItem;
+
+    }
+
 }
